d5e09a2818b07de29ace51a2c5d4e6085e07d62a
Andrew Peel <blenderpro3d@gmail.com>
2020-05-26 17:25:42 -0700

    Update Version Number

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "PyClone",
     "author": "Andrew Peel",
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "blender": (2, 83, 0),
     "location": "3D Viewport Sidebar and File Browser",
     "description": "This is an asset management and development engine",
