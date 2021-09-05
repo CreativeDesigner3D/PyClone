f9f967d0b315b810079a45200197875ce3f2c9c4
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2021-09-05 10:32:52 -0700

    Allow Default Drop Functionality when Dropping Blend Files

--- ops/pc_window_manager.py
+++ ops/pc_window_manager.py
@@ -26,6 +26,10 @@ class WM_OT_drag_and_drop(bpy.types.Operator):
         scene_props = pyclone_utils.get_wm_props(context.scene)
 
         print('FILEPATH',self.filepath)
+        filename, ext = os.path.splitext(self.filepath)
+        if ext == '.blend':
+            bpy.ops.wm.drop_blend_file('INVOKE_DEFAULT',filepath=self.filepath)
+            return {'FINISHED'}
         if self.filepath == 'Error':
             path = pyclone_utils.get_file_browser_path(context)
             lib = wm_props.libraries[scene_props.active_library_name]
@@ -33,7 +37,6 @@ class WM_OT_drag_and_drop(bpy.types.Operator):
         elif scene_props.active_library_name in wm_props.libraries:
             lib = wm_props.libraries[scene_props.active_library_name]
             eval('bpy.ops.' + lib.drop_id + '("INVOKE_DEFAULT",filepath=self.filepath)')
-
         return {'FINISHED'}
 
 classes = (
