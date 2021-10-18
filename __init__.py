03b430e943feecce532043f48e7b08edb8fa79f4
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2021-10-18 14:16:37 -0700

    Updating Version Number

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "PyClone",
     "author": "Andrew Peel",
-    "version": (0, 5, 2),
+    "version": (0, 5, 3),
     "blender": (2, 93, 0),
     "location": "3D Viewport Sidebar and File Browser",
     "description": "This is an asset management and development engine",
